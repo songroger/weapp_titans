6a6af06c20802ee1e2f4191c203fb77f4a574834
songroger <songroger@vip.qq.com>
2016-10-10 14:21:15 +0800

    up

--- app/config.js
+++ app/config.js
@@ -1,5 +1,8 @@
+/*
+ *模块只有通过 module.exports 才能对外暴露接口.在需要使用这些模块的文件中，使用 require(path) 将公共代码引入。
+ */
 module.exports = {
     /** 通讯域名 */
     host: '120.26.42.111',
-    basePath: '',
+    basePath: '/weapp',
 };
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,15 +1,45 @@
+const api = require('../../lib/api.js');
+var inittitle = "沁园春·雪"
 var initData = '北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。山舞银蛇，原驰蜡象，欲与天公试比高。须晴日，看红装素裹，分外妖娆。\n\n江山如此多娇，引无数英雄竞折腰。惜秦皇汉武，略输文采；唐宗宋祖，稍逊风骚。一代天骄，成吉思汗，只识弯弓射大雕。俱往矣，数风流人物，还看今朝。'
 var extraLine = [];
+// var reg=new RegExp("/<p>(.*?)<\/p>/g","gmi");
 Page({
     //content data
     data: {
-        text: initData
+        text: "",
+        title:"",
+        author:""
     },
     onLoad: function(options) {
         // Do some initialize when page load.
     },
     onShow: function() {
         // Do something when page show.
+        var that = this
+        wx.request({
+            url: api.getUrl('/random_poem'),
+            data: {
+            },
+            header: {
+                'Content-Type': 'application/json'
+            },
+            success: function(res) {
+                // console.log(res.data)
+                that.setData({
+                    title: res.data.title,
+                    text: res.data.content.replace(/<p>(.*?)<\/p>/g,"$1"),
+                    author: "\n"+ res.data.author + "("+ res.data.dynasty + ")",
+                    intro: "赏析："+ res.data.intro.replace(/<p>(.*?)<\/p>/g,"$1")
+
+                })
+            },
+            fail: function(res) {
+                that.setData({
+                    title: inittitle,
+                    text: initData
+                })
+            }
+        })
     },
     // go to tab page
     gotoTab() {
