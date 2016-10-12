85c1ffe44d4dbb24abd6aaa55df309a9a822a60e
songroger <songroger@vip.qq.com>
2016-10-12 18:41:34 +0800

    module

--- app/lib/util.js
+++ app/lib/util.js
@@ -1,3 +1,54 @@
+/*
+ *Created by songroger on Oct.12.2016.
+ */
+const api = require('api.js');
+var inittitle = "沁园春·雪"
+var initData = '北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。山舞银蛇，原驰蜡象，欲与天公试比高。须晴日，看红装素裹，分外妖娆。\n\n江山如此多娇，引无数英雄竞折腰。惜秦皇汉武，略输文采；唐宗宋祖，稍逊风骚。一代天骄，成吉思汗，只识弯弓射大雕。俱往矣，数风流人物，还看今朝。'
+var extraLine = [];
+function loadBunchData(callback) {
+    // load Bunch data
+    wx.request({
+        url: api.getUrl('/random_poem'),
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res) {
+            // console.log(res.data)
+            callback(res.data)
+        },
+        fail: function() {
+            var res = {};
+            res.data = {};
+            res.data.title = inittitle;
+            res.data.text = initData;
+            callback(res.data)
+        }
+    })
+
+}
+
+function loadPieceData(callback) {
+    // load piece data
+    wx.request({
+        url: api.getUrl('/random_line'),
+        header: {
+            'Content-Type': 'application/json'
+        },
+        success: function(res) {
+            // console.log(res.data)
+            callback(res.data)
+        },
+        fail: function() {
+            var res = {};
+            res.data = {};
+            res.data.title = inittitle;
+            res.data.text = initData;
+            callback(res.data)
+        }
+    })
+}
+
+
 module.exports = {
     // 一维数组转二维数组
     listToMatrix(list, elementsPerSubArray) {
@@ -20,4 +71,6 @@ module.exports = {
         promise.then(callback, callback);
         return promise;
     },
+    loadBunchData: loadBunchData,
+    loadPieceData:loadPieceData,
 };
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,4 +1,5 @@
 const api = require('../../lib/api.js');
+const util = require('../../lib/util.js');
 var inittitle = "沁园春·雪"
 var initData = '北国风光，千里冰封，万里雪飘。望长城内外，惟余莽莽；大河上下，顿失滔滔。山舞银蛇，原驰蜡象，欲与天公试比高。须晴日，看红装素裹，分外妖娆。\n\n江山如此多娇，引无数英雄竞折腰。惜秦皇汉武，略输文采；唐宗宋祖，稍逊风骚。一代天骄，成吉思汗，只识弯弓射大雕。俱往矣，数风流人物，还看今朝。'
 var extraLine = [];
@@ -10,42 +11,28 @@ Page({
         title:"",
         author:""
     },
-    onLoad: function(options) {
-        // Do some initialize when page load.
-    },
-    onShow: function() {
+    onShow: function(options) {
         // Do something when page show.
+    },
+    onLoad: function() {
+        // Do some initialize when page load.
         var that = this
         var appInstance = getApp()
         var rdn = Math.ceil(Math.random()*4)
         this.setData({
             top_style: "background-image: url(../../images/bg" +rdn+".jpg);"
         })
-        wx.request({
-            url: api.getUrl('/random_poem'),
-            data: {
-            },
-            header: {
-                'Content-Type': 'application/json'
-            },
-            success: function(res) {
-                // console.log(res.data)
-                that.setData({
-                    title: res.data.title,
-                    text: res.data.content.replace(/<p>(.*?)<\/p>/g,"$1"),
-                    author: "\n"+ res.data.author + "("+ res.data.dynasty + ")",
-                    intro: "赏析："+ (res.data.intro.replace(/<p>(.*?)<\/p>/g,"$1") ||"暂无，先看看其他内容吧~")
-                }),
-                wx.setStorageSync('poem_text', res.data.content.replace(/<p>(.*?)<\/p>/g,"$1")),
-                wx.setStorageSync('poem_title', res.data.title)
-            },
-            fail: function(res) {
-                that.setData({
-                    title: inittitle,
-                    text: initData
-                })
-            }
-        })
+        util.loadBunchData(function(data){
+              // console.log(data);
+              that.setData({
+                title: data.title,
+                text: data.content.replace(/<p>(.*?)<\/p>/g,"$1"),
+                author: "\n"+ data.author + "("+ data.dynasty + ")",
+                intro: "赏析："+ (data.intro.replace(/<p>(.*?)<\/p>/g,"$1") ||"暂无，先看看其他内容吧~")
+              }),
+              wx.setStorageSync('poem_text', data.content.replace(/<p>(.*?)<\/p>/g,"$1")),
+              wx.setStorageSync('poem_title', data.title)
+        }); 
     },
     // go to detail page
     gotoTab() {
@@ -53,5 +40,22 @@ Page({
     },
     onPullDownRefresh: function() {
         // Do something when pull down
+        var that = this
+        var rdn = Math.ceil(Math.random()*4)
+        this.setData({
+            top_style: "background-image: url(../../images/bg" +rdn+".jpg);"
+        })
+        util.loadBunchData(function(data){
+              // console.log(data);
+              that.setData({
+                title: data.title,
+                text: data.content.replace(/<p>(.*?)<\/p>/g,"$1"),
+                author: "\n"+ data.author + "("+ data.dynasty + ")",
+                intro: "赏析："+ (data.intro.replace(/<p>(.*?)<\/p>/g,"$1") ||"暂无，先看看其他内容吧~")
+              }),
+              wx.setStorageSync('poem_text', data.content.replace(/<p>(.*?)<\/p>/g,"$1")),
+              wx.setStorageSync('poem_title', data.title)
+        }); 
+        wx.stopPullDownRefresh()
     },
 });
