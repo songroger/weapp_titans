5fc1843a386c9c103e9811724d42bb17f7266787
songroger <songroger@vip.qq.com>
2016-10-13 14:27:45 +0800

    title

--- app/pages/tab/tab.js
+++ app/pages/tab/tab.js
@@ -7,6 +7,12 @@ Page({
   onShow: function(options) {
     // Do something when page show.
     },
+  onReady: function() {
+    // Do something when page ready.一个页面只会调用一次，代表页面已经准备妥当，可以和视图层进行交互.对界面的设置如wx.setNavigationBarTitle请在onReady之后设置。
+    // wx.setNavigationBarTitle({
+    //   title: ""
+    // })
+  },
   onLoad: function() {
         // Do some initialize when page load.
         var that = this
